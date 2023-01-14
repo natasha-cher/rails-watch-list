9f81d19477d361c7993085083e984eb279030fca
Natalia Chernenko <natchernenko13@gmail.com>
2023-01-14 19:58:56 +0900

    added bookmark controller and fixed the views

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    if @bookmark.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.list = @list
+    @bookmark.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
