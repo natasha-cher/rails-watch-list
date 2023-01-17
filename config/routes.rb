ce925199d1c676bf644a206b022f1df19f74bdf8
Natalia Chernenko <natchernenko13@gmail.com>
2023-01-17 20:40:27 +0900

    change platform in gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'lists#index', as: 'home'
   resources :lists, only:[ :index, :show, :new, :create ] do
     resources :bookmarks, only:[ :new, :create, :destroy]
   end
