c686015a276cd9c24d17c57f0f90f20acb676e38
Natalia Chernenko <natchernenko13@gmail.com>
2023-01-19 19:52:32 +0900

    fixed the banner

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ response['results'].each do |movie_hash|
   puts
   Movie.create!(
     poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
-    title: movie_hash['original_title'],
+    title: movie_hash['title'],
     overview: movie_hash['overview'],
     rating: movie_hash['vote_average']
   )
